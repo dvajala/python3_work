3ff4697ecb2055f15845956820b848e7bf128bc1
dinesh-vajala <dineshvajala@gmail.com>
2021-10-08 06:19:52 -0500

    vars & strings

---
+++ apostrophe.py
@@ -0,0 +1,13 @@
+message = "One of Python's strengths is its diverse community."
+
+print(message)
+
+name = 'diNesH vaJala'
+
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+message2 = f"Hello {name.title()}!, would you like to learn some python today?"
+
+print(message2)
