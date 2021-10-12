35491e01da89396dba2bb9a8ef2d8c640d5679ed
dinesh-vajala <dineshvajala@gmail.com>
2021-10-11 19:12:22 -0500

    more dictionaries

--- dictionaries.py
+++ dictionaries.py
@@ -1,19 +1,8 @@
-alien_0 = {'color': 'green',"points":5}
-
-print(alien_0)
-
-alien_0['color'] = 'yellow'
-
-print(alien_0)
-
-del alien_0['points']
-
-print(alien_0)
-
 languages = {
-    'dinesh': 'python',
-    'veda': 'youtube',
-    'anu': 'nvivo'
-}
-language = languages.get('sarah','No language')
-print(f"Veda's favorite language is {language.title()}")
+    'john': 'python',
+    'jack': 'c',
+    'jimmy': 'python',
+    'sarah': 'ruby',
+    'jerry': 'python'
+    }
+print(sorted(set(languages.values())))
