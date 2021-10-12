b4259e3de33c69893b23fc6405ee058fbcff8bb7
dinesh-vajala <dineshvajala@gmail.com>
2021-10-11 19:28:10 -0500

    more dictionaries

--- dictionaries.py
+++ dictionaries.py
@@ -1,8 +1,13 @@
-languages = {
-    'john': 'python',
-    'jack': 'c',
-    'jimmy': 'python',
-    'sarah': 'ruby',
-    'jerry': 'python'
-    }
-print(sorted(set(languages.values())))
+aliens = []
+
+for num in range(30):
+    new_alien = {'color':'green', 'points': 5, 'speed': 'slow'}
+    aliens.append(new_alien)
+
+for alien in aliens[:3]:
+    if alien['color'] == 'green':
+        alien['color'] = 'yellow'
+        alien['points'] = 10
+        alien['speed'] = 'medium'
+
+print(aliens[:4])
