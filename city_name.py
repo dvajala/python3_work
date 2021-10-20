f95807e383ec46659a7e6973d0bbc3077173c44e
dinesh-vajala <dineshvajala@gmail.com>
2021-10-19 20:30:33 -0500

    even more functions

--- city_name.py
+++ city_name.py
@@ -4,11 +4,20 @@ def city_country(city,country):
 
 
 
-def make_album(artist,album):
+def make_album(artist,album,songs=None):
     album_dict = {'artist':artist,'album':album}
+    if songs:
+        album_dict['songs'] = songs
     return album_dict
 
-album1 = make_album('santiago','chile')
-album2 = make_album('Beatles','White Album')
-album3 = make_album('Rolling stones','ruby tuesday')
-print(album1,album2,album3)
+while True:
+    print("Tell me the artist and album name\nEnter 'q' to quit at any time")
+    artist = input("Enter artist name: ")
+    if artist == 'q':
+        break
+    album = input("Enter album name: ")
+    if album == 'q':
+        break
+
+    my_dict = make_album(artist,album)
+    print(my_dict)
