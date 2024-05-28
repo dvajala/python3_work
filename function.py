81acfafa0085a4db434eac30e2dc7a057799e3f5
Dinesh Vajala <dineshvajala@gmail.com>
2024-05-27 22:30:21 -0500

    Update function.py

--- function.py
+++ function.py
@@ -2,9 +2,9 @@ import random
 from sty import fg
 
 def generateRGB():
-    red = random.randint(0,256)
-    green = random.randint(0,256)
-    blue = random.randint(0,256)
+    red = random.randint(0,255)
+    green = random.randint(0,255)
+    blue = random.randint(0,255)
     return red,green,blue
 
 def generateColor(red, green, blue):
@@ -12,6 +12,6 @@ def generateColor(red, green, blue):
 
 red, green, blue = generateRGB()
 color = generateColor(red, green, blue)
-print(color, "Test string with random colors")
+print(color, "Test string with random colors" + rs.all)
 
 
